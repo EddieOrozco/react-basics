06b0e7cfb3b2b948a3abfcecbc376a1410c43006
EddieOrozco <orozcoe230@gmail.com>
2021-05-08 08:43:26 -0500

    Creating Variables & Fuctions displaying with {} inside JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Setting Variables
+let name = 'Eddie';
+let lname = ' Orozco';
+let fullName = name + lname;
+let age = 29;
+let loggedIn = false;
+let width = 400;
+let height = 300;
+let styles = {
+  box: {
+    background: 'red',
+    color: '#fff',
+    textAlign: 'center',
+    width: `${(width * 3) / 2}px`,
+    height: `${height}px`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  h1: {
+    background: 'black',
+    width: '100px',
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '.8rem',
+  },
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // Functions
+  let showAccount = () => {
+    return <div>You are logged in</div>;
+  };
+  return (
+    <>
+      <div style={styles.box} className='box-main'>
+        {/* Conditionals (Ternary) */}
+        Welcome {fullName} you age is {age}
+        {loggedIn ? showAccount() : ''}
+        <h1 style={styles.h1} className='main-box'>
+          Testing
+        </h1>
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
